acdf93e517a574c8942173f1795dd6c8563cd358
Horiane <landerff167@gmail.com>
2023-03-02 01:07:08 +0700

    1.2
    Major bug fixes and better comments

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-#made by u/icantnotbreathe
+#made by u/icantnotbreathe, suggestions by u/drift7rs
 #feel free to copy!
 #imports
 from math import trunc
+from sys import exit
 
 #inputs
-playerRank=int(input("Your current rank:"))
-goalRank=int(input("Your target rank:"))
-avgXP=int(input("Your average xp per match:"))
-avgHours=int(input("Your average hours per day:"))
+playerRank=int(input("Your current rank: "))
+goalRank=int(input("Your target rank: "))
+avgXP=int(input("Your average xp per match: ")) #average xp earned per game
+avgHours=int(input("Your average hours per day: ")) #time played each day
 
-totalXPneeded=0
+if avgHours>24:
+    print("How did you play phantom forces more than 24 hours a day?")
+    exit()
+
+totalXPneeded=0 #declare varible to avoid complier issues
+i=playerRank #Setting the starting rank to calculate
 
 #calculation
-for i in range(goalRank-playerRank):
-    i=playerRank
-    totalXPneeded=totalXPneeded+((i+1)*1000)
+for i in range(goalRank-playerRank): #loop to calc the xp needed by going from player rank - goal rank
+    totalXPneeded=totalXPneeded+((i+1)*1000) #a "equation"
     i=+1
-matchestgt=totalXPneeded/avgXP
-timetgt=matchestgt*900
-int(timetgt)
+matchesNeeded=totalXPneeded/avgXP #matches needed
+timeNeeded=matchesNeeded*930 #15 minutes=900 seconds + 30 seconds intermission
 
-realtime=(timetgt/3600)/avgHours
+realtime=(timeNeeded/3600)/avgHours #calc days needed to realisticly get to targeted rank
 
-days=timetgt//86400
-hours=(timetgt-(days*86400))//3600
-minutes=(timetgt-(days*86400)-(hours*3600))//60
+#making seconds into days hours minutes
+days=timeNeeded//86400
+hours=(timeNeeded-(days*86400))//3600
+minutes=(timeNeeded-(days*86400)-(hours*3600))//60
 
+#"rounding" the numbers
 days=trunc(days)
 hours=trunc(hours)
 minutes=trunc(minutes)
@@ -33,6 +39,7 @@ minutes=trunc(minutes)
 #output
 print(f"{realtime} days of grinding")
 
+#i hate this if if if mess, its basicly just displaying required parameters
 if minutes==0:
     print(f"{days} days and {hours} hours of continuous grinding")
 elif hours==0:
